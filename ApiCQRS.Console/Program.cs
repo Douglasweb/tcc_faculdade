05b92df8edde2c526b7ed571f281baa628cdeb94
Douglas <webdouglasti@gmail.com>
2021-10-20 13:20:24 -0300

    Adicionado console a aplicacao

---
+++ ApiCQRS.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiCQRS.ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
